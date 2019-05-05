5babd7263e6b72a12920140335512c64291f9abc
shilei <18435156033@163.com>
2019-05-05 23:56:43 +0800

    添加线上地址

--- app/src/main/java/com/uplan/miyao/net/Api.java
+++ app/src/main/java/com/uplan/miyao/net/Api.java
@@ -4,5 +4,5 @@ package com.uplan.miyao.net;
 public class Api {
 
 //   public static final String BASE_URL = "http://22ju570648.iok.la";
-   public static final String BASE_URL = "http://22ju570648.iok.la";
+   public static final String BASE_URL = "http://www.51mix.cn";
 }
